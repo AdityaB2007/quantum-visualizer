b04a0b751b2f5fc1eb587cb7034a4493785fdb0d
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-04 23:10:17 -0500

    feat: add complex valued wavepacket function

--- src/gaussian_wavepackets.py
+++ src/gaussian_wavepackets.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-def gaussian_wavepacket(x, x0, spread):
-    psi = np.exp(-1 * (x - x0) ** 2 / (2 * spread ** 2))
+def gaussian_wavepacket(x, x0, spread, k):
+    psi = np.exp(-1 * (x - x0) ** 2 / (2 * spread ** 2)) * np.exp(1j * k * x)
     return psi
