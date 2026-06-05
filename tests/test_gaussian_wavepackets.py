85e0d53eab35968613eaa41e7ef25d5a7a82bff4
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-05 11:20:17 -0500

    feat: account for complex psi

--- tests/test_gaussian_wavepackets.py
+++ tests/test_gaussian_wavepackets.py
@@ -8,5 +8,5 @@ def test_gaussian_wavepackets():
     k = 0
     x = np.linspace(x0 - 5 * spread, x0 + 5 * spread, 1000)
     psi = gaussian_wavepacket(x, x0, spread, k)
-    max_idx = np.argmax(psi)
+    max_idx = np.argmax(np.abs(psi))
     assert abs(x[max_idx] - x0) < 0.01
