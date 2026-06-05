5043082c8b46e9fb6580625dcbf0f624164d660d
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-05 11:17:30 -0500

    fix: augment testing

--- tests/test_gaussian_wavepackets.py
+++ tests/test_gaussian_wavepackets.py
@@ -3,11 +3,10 @@ import numpy as np
 from src.gaussian_wavepackets import gaussian_wavepacket
 
 def test_gaussian_wavepackets():
-    x = np.linspace(-100, 100, 1E10)
     x0 = 0
     spread = 0.2
     k = 0
+    x = np.linspace(x0 - 5 * spread, x0 + 5 * spread, 1000)
     psi = gaussian_wavepacket(x, x0, spread, k)
     max_idx = np.argmax(psi)
     assert abs(x[max_idx] - x0) < 0.01
-    assert abs(np.exp(1j * k * x)) == 1
