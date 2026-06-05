3c48a53b45e2645ef03151f38bdfad0061956064
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-04 22:35:58 -0500

    fix: increase sample size on gaussian

--- tests/test_gaussian_wavepackets.py
+++ tests/test_gaussian_wavepackets.py
@@ -3,7 +3,7 @@ import numpy as np
 from src.gaussian_wavepackets import gaussian_wavepacket
 
 def test_gaussian_wavepackets():
-    x = np.linspace(-5, 5, 1000)
+    x = np.linspace(-100, 100, 1E10)
     x0 = 0
     spread = 0.2
     psi = gaussian_wavepacket(x, x0, spread)
