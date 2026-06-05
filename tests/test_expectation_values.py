0519a0465b639c09a7981056458f1d154e2f6ed0
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-05 12:49:04 -0500

    feat: add variance testing

--- tests/test_expectation_values.py
+++ tests/test_expectation_values.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from src.particle_in_box import particle_in_box_wavefunction
-from src.expectation_values import expectation_x
+from src.expectation_values import expectation_x, expectation_x_squared, variance_x
 
 def test_expectation_x():
     L = 1
@@ -11,3 +11,18 @@ def test_expectation_x():
     psi = particle_in_box_wavefunction(x, n, L)
     result = np.abs(expectation_x(x, psi) - L / 2)
     assert result < 1e-8
+
+def test_variance_x():
+    L = 1
+    n = 1
+    x = np.linspace(0, L, 1000)
+
+    psi = particle_in_box_wavefunction(x, n, L)
+
+    mean_x = expectation_x(x, psi)
+    mean_x2 = expectation_x_squared(x, psi)
+    var = variance_x(x, psi)
+
+    assert mean_x2 > mean_x ** 2
+    assert var > 0
+    assert abs(var - (mean_x2 - mean_x ** 2)) < 1e-8
