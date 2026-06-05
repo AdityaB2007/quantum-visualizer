5cddc1a6d8ea50ccbdde35419c40cc5e75df3c37
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-05 11:10:12 -0500

    feat: add complex valued wavepacket testing

--- tests/test_gaussian_wavepackets.py
+++ tests/test_gaussian_wavepackets.py
@@ -6,6 +6,8 @@ def test_gaussian_wavepackets():
     x = np.linspace(-100, 100, 1E10)
     x0 = 0
     spread = 0.2
-    psi = gaussian_wavepacket(x, x0, spread)
+    k = 0
+    psi = gaussian_wavepacket(x, x0, spread, k)
     max_idx = np.argmax(psi)
     assert abs(x[max_idx] - x0) < 0.01
+    assert abs(np.exp(1j * k * x)) == 1
