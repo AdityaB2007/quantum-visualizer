4790566385f85ce86e961c802663f42728e2184d
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-04 22:31:07 -0500

    feat: add model testing for gaussian

---
+++ tests/test_gaussian_wavepackets.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from src.gaussian_wavepackets import gaussian_wavepacket
+
+def test_gaussian_wavepackets():
+    x = np.linspace(-5, 5, 1000)
+    x0 = 0
+    spread = 0.2
+    psi = gaussian_wavepacket(x, x0, spread)
+    max_idx = np.argmax(psi)
+    assert abs(x[max_idx] - x0) < 0.01
