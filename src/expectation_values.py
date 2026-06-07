5e7679d742418551daee890b9788df23c864a9e0
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-06-07 16:23:49 -0500

    feat: add momentum variance and uncertainty code

--- src/expectation_values.py
+++ src/expectation_values.py
@@ -21,4 +21,21 @@ def expectation_p(x, psi, hbar=1):
     p_operator_psi = -1j * hbar * np.gradient(psi, x)
     integrand = np.conjugate(psi) * p_operator_psi
     exp_p = np.trapezoid(integrand, x)
-    return exp_p
+    return np.real(exp_p)
+
+def expectation_p_squared(x, psi, hbar=1):
+    dpsi_dx = np.gradient(psi, x)
+    d2psi_dx2 = np.gradient(dpsi_dx, x)
+    p_squared_psi = -(hbar ** 2) * d2psi_dx2
+    integrand = np.conjugate(psi) * p_squared_psi
+    exp_p2 = np.trapezoid(integrand, x)
+    return np.real(exp_p2)
+
+def variance_p(x, psi, hbar=1):
+    mean_p = expectation_p(x, psi, hbar)
+    mean_p2 = expectation_p_squared(x, psi, hbar)
+    return mean_p2 - mean_p ** 2
+
+def uncertainty_p(x, psi, hbar=1):
+    var_p = variance_p(x, psi, hbar)
+    return np.sqrt(var_p)
